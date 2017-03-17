d98a6850109c0e1fa80fc0b18a4be0f8e62a832f
Betty Baker <betty@heysparkbox.com>
2017-03-17 11:20:34 -0400

    Use a function to say Hello World in JavaScript

--- hello-javascript.js
+++ hello-javascript.js
@@ -3,3 +3,10 @@ hello = {
 }
 key = "message"
 console.log(hello[key]); // Bracket Notation -- Computed Accessor
+
+function greeting(){
+  var name = "Betty";
+  console.log(`Hello ${name}!`); // these are backticks (`), they are on tilde key
+}
+
+greeting();
